7765bc22e8919bef90bdb6514d568ec7957b9285
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-12-23 15:34:31 -0600

    deleting files

--- public/address_book.php
+++ public/address_book.php
@@ -1,26 +1,73 @@
 <?php
 
-$addressBook = [
-    ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
-    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
-    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
-];
+// $addressBook = [
+//     ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
+//     ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
+//     ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
+// ];
+//var_dump($addressBook);
+$fileName = 'address_book.csv';
 
-$handle = fopen('address_book.csv', 'w');
 
-foreach ($addressBook as $row) {
-    fputcsv($handle, $row);
+function saveFile($fileName, $array){
+    //open a file that you named
+    $handle = fopen($fileName, 'w');
+    //foreach loop to write each new array item
+    foreach ($array as $value) {
+    //write each item into the file name you entered
+       fputcsv($handle, $value);
+    }
+    // echo 'file saved' . PHP_EOL;
+    fclose($handle);
+    	
+	}
 
-}
+function read_Address_Book($fileName){
+			$handle = fopen($fileName, 'r');
 
-fclose($handle);
+			$addressBook = [];
+
+			while(!feof($handle)) {
+				
+				$row = fgetcsv($handle);
+				
+				if (!empty($row)) {
+					$addressBook[] = $row;
+				}
+			}
+			return $addressBook;
+		fclose($handle);
+	
+	}
+
+$addressBook = read_Address_Book($fileName);
+
+// function openFile($fileName){
+// 			$contents_Array = [];
+		
+// 		if(filesize($fileName) > 0) {
+// 			$handle = fopen($fileName, 'r');
+// 			$contents = trim(fread($handle, filesize($fileName)));
+// 			//$contents_Array = explode("\n", $contents);
+// 			fclose($handle);
+// 		}
+// 			return $contents_Array;
+// 	}
 
 //Check for POST request 
-
-
-		//add data from form to address book
-
-
+if (!empty($_POST)) {
+		
+		$addressBook[] = $_POST;
+		saveFile($fileName, $addressBook);
+	}	
+//add data from form to address book
+if(isset($_GET)) {
+	$id = $_GET['remove'];
+	unset($addressBook[$id]);
+	saveFile('address_book.csv',$addressBook);
+}
+		
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,17 +85,18 @@ fclose($handle);
 			</tr>
 			<!-- start working with php, and echoing out the data from $addressBook -->
 			
-			<?foreach ($addressBook as $entry): ?>
+			<?foreach ($addressBook as $key => $entry): ?>
 				<tr>
 					<?php foreach ($entry as $value): ?>
 						<td><?= $value ?></td>
 					<?php endforeach ?>
+						<td><a href="/address_book.php?remove=<?= $key ?>">X</a></td>
 				
 				</tr>
 			<? endforeach; ?>
 
 		</table>
-		<form>
+		<form method="Post" action="/address_book.php">
 			<div>
 				<input name="header" type="text" placeholder="header">
 				<input name="Address" type="text" placeholder="Address">
@@ -56,7 +104,7 @@ fclose($handle);
 				<input name="State" type="text" placeholder="State">
 				<input name="Zip" type="text" placeholder="Zipcode">
 
-				<button name="Submit" type="Submit" value="Submit">Submit</button>
+				<button  type="Submit" value="Submit">Submit</button>
 			</div>
 		</form>
 	</body>
