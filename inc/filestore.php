7addc8a221b3eaf22fe8b3cdf7fd3a41972599ee
Paul Love <paullove@Pauls-MacBook-Pro.local>
2015-01-20 15:33:42 -0600

    afternoon project

---
+++ inc/filestore.php
@@ -0,0 +1,44 @@
+<?php
+
+
+ class Filestore
+{
+    public $filename;
+
+    function __construct($filename)
+    {
+        // Sets $this->filename
+    }
+
+    /**
+     * Returns array of lines in $this->filename
+     */
+    function readLines()
+    {
+
+    }
+
+    /**
+     * Writes each element in $array to a new line in $this->filename
+     */
+    function writeLines($array)
+    {
+
+    }
+
+    /**
+     * Reads contents of csv $this->filename, returns an array
+     */
+    function readCSV()
+    {
+
+    }
+
+    /**
+     * Writes contents of $array to csv $this->filename
+     */
+    function writeCSV($array)
+    {
+
+    }
+}
